201b1c5b68fcb1bbbc0d1de542c494950bbffde5
alessandrostein <alessandro@alessandrostein.com>
2017-09-12 00:12:36 -0300

    Vue playground

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <Postslist v-bind:source="source"></Postslist>
+  </div>
+</template>
+
+<script>
+
+import Postslist from './components/Postslist'
+
+export default {
+  name: 'app',
+  components: {
+    Postslist
+  },
+  data () {
+    return {
+      source: ''
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  padding-top: 20px
+}
+</style>
---
+++ src/components/Postslist.vue
@@ -0,0 +1,41 @@
+<template>
+
+  <div class="postslist">
+    <div class="container">
+      <h1>Listagem de Posts</h1>
+      <ul>
+        <li v-for="post in posts">{{post.title}}</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'postslist',
+  data () {
+    return {
+      posts: []
+    }
+  },
+
+  created: function () {
+    this.$http.get('http://jsonplaceholder.typicode.com/posts')
+      .then(response => {
+        console.log(response);
+        this.posts = response.data;
+      });
+  }
+}
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .media-object {
+    width: 128px;
+    padding: 10px;
+  }
+  .media {
+    border-top: 1px solid lightgray;
+    padding-top: 20px;
+  }
+</style>
